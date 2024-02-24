9bd38a98d3c2fbe402fb0ad31d00978f9356e79b
sh <sh@DESKTOP-8J54UUA>
2024-02-24 15:23:07 -0800

    i have commite pr1 and pr2

---
+++ src/project2/Pr1.java
@@ -0,0 +1,10 @@
+
+package project2;
+
+public class Pr1 {
+	public static void main (String args[] ) {
+		
+	//aaaaa
+	}
+
+}
---
+++ src/project2/Pr2.java
@@ -0,0 +1,11 @@
+package project2;
+
+public class Pr2 {
+	
+	
+	public static void main (String args[]) {
+		
+	//	rrrrr
+	}
+
+}
